577dd7191ca19d660efd5f76445a32e90fc4eee4
husitech <admin@husi.ca>
2025-04-10 23:58:49 -0400

    frontend

---
+++ script.js
@@ -0,0 +1,6 @@
+// Basic JavaScript for alert functionality
+// In a real app, this would be replaced with dynamic data
+
+function showTradeDetails(trade) {
+    alert(`Trade Suggestion: ${trade}`);
+}
